3b11f6d253964d81c574b4c9279237bf68353ef9
ia.sazhinov@tensor.ru <ia.sazhinov@tensor.ru>
2024-04-05 18:51:31 +0300

    Добавил navbar https://youtu.be/GNrdg3PzpJQ?t=8330

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 import './styles/App.css'
@@ -7,6 +7,13 @@ export default function App() {
 
     return (
         <BrowserRouter>
+            <div className="navbar">
+                <div className="navbar__links">
+                    <Link to="/about">О сайте</Link>
+                    <Link to="/posts">Посты</Link>
+                </div>
+            </div>
+
             <Routes>
                 <Route path="/about" element={<About />} />
                 <Route path="/posts" element={<Posts />} />
